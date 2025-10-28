f8da3e21fb04b3f25de52902b2e336e7412fcf39
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-28 10:43:52 -0700

    Add operate function

--- script.js
+++ script.js
@@ -19,5 +19,13 @@ function division(num1, num2) {
 }
 
 function operate(num1, num2, operator){
-    
+    if (operator === '+'){
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1,num2);
+    } else if (operator === '*') {
+        return multiply(num1,num2);
+    } else if (operator === '/') {
+        return division(num1,num2);
+    }
 }
