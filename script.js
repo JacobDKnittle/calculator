54acee6cb881f7edc99dd6b0dba9f27fd4acc54d
Jacob Knittle <jacobdknittle@gmail.com>
2025-10-29 09:57:09 -0700

    Add conditional for adding operator to screen

--- script.js
+++ script.js
@@ -1,54 +1,64 @@
 let num1;
 let operator;
 let num2;
-let operatorValues = ['+', '-', 'x', '/']
+let operatorValues = ["+", "-", "x", "/"];
 
-const buttonContainer = document.querySelector('.buttons-container');
-const screen = document.querySelector('input');
+const buttonContainer = document.querySelector(".buttons-container");
+const screen = document.querySelector("input");
 
 screen.value = 0;
 
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 
 function subtract(num1, num2) {
-    return num1 - num2;
+  return num1 - num2;
 }
 
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
 
 function division(num1, num2) {
-    return num1 / num2;
+  return num1 / num2;
 }
 
 // calls different operator functions based on user input
-function operate(num1, num2, operator){
-    if (operator === '+'){
-        return add(num1, num2);
-    } else if (operator === '-') {
-        return subtract(num1,num2);
-    } else if (operator === 'x') {
-        return multiply(num1,num2);
-    } else if (operator === '/') {
-        return division(num1,num2);
-    }
+function operate(num1, num2, operator) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "x") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return division(num1, num2);
+  }
 }
 
 // captures user clicks on calculator
-buttonContainer.addEventListener(('click'), (e) => {
-    if (e.target.textContent.length > 1){
-        return;
-    }
-    let buttonPressed = e.target.textContent;
-    console.log(buttonPressed);
-    manageUserInput(buttonPressed)
-})
+buttonContainer.addEventListener("click", (e) => {
+  if (e.target.textContent.length > 1) {
+    return;
+  }
+  let buttonPressed = e.target.textContent;
+  console.log(buttonPressed);
+  manageUserInput(buttonPressed);
+});
 
+// manages user input whether to display to screen
 function manageUserInput(buttonPressed) {
-    if (typeof num1 === 'undefined' && Number.isInteger(+buttonPressed)) {
-        screen.value = buttonPressed;
-    }
+  if (typeof num1 === "undefined" && Number.isInteger(+buttonPressed)) {
+    num1 = buttonPressed;
+    screen.value = `${num1}`;
+  } else if (
+    typeof num1 !== "undefined" &&
+    typeof operator === "undefined" &&
+    typeof num2 === "undefined" &&
+    operatorValues.includes(buttonPressed)
+  ) {
+    operator = buttonPressed;
+    screen.value = `${num1} ${operator}`;
+  }
 }
